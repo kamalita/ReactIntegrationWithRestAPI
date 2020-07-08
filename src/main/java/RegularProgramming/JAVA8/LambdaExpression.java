cc2a40d40410b126451e044414ce68ae83f5b84c
kamalita <kamalitabiswas2015@gmail.com>
2020-07-08 17:32:46 +0530

    added LambdaExpression class

---
+++ src/main/java/RegularProgramming/JAVA8/LambdaExpression.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020.
+ * Kamalita's coding
+ */
+
+package RegularProgramming.JAVA8;
+
+public class LambdaExpression {
+    public static void main(String[] args) {
+        Integer input=new Integer(456);
+            System.out.println(input);
+    }
+}
