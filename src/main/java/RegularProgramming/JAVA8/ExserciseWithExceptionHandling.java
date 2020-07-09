1085df9093acfddeb0e3ae7e5a8711536dea2c0d
kamalita <kamalitabiswas2015@gmail.com>
2020-07-09 20:57:23 +0530

    added Exception for lambda class

---
+++ src/main/java/RegularProgramming/JAVA8/ExserciseWithExceptionHandling.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2020.
+ * Kamalita's coding
+ */
+
+package RegularProgramming.JAVA8;
+
+import java.util.function.BiConsumer;
+
+public class ExserciseWithExceptionHandling {
+    public static void main(String[] args) {
+        int[] numbers={1,6,3,41,5};
+        int key=0;
+        try {
+            check(numbers, key, (v, k) -> System.out.println(v / k));
+        }
+        catch (ArithmeticException arithmeticException){
+            arithmeticException.getStackTrace();
+        }
+    }
+
+    public static void check(int[] numbers, int key, BiConsumer<Integer,Integer> consumer){
+        for (int i:numbers) {
+            consumer.accept(i,key);
+        }
+    }
+}
