be46361c862f46038848e192a1a96859b3eb3700
kamalita <kamalitabiswas2015@gmail.com>
2020-07-12 16:11:52 +0530

    added Interface implementation

---
+++ src/main/java/RegularProgramming/JAVA8/ICheckImplementaion.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020.
+ * Kamalita's coding
+ */
+
+package RegularProgramming.JAVA8;
+
+public interface ICheckImplementaion {
+     void checkAbstract();
+    static void checkImplementation(){
+        System.out.println("not so abstract");
+    }
+}
